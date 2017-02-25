9c2f2d4c4188f4c238549000e50ddf374ac2c1ca
Leonard Shi <leonard.shi.cs@gmail.com>
2017-02-25 18:21:48 +0800

    Critical fix for bluebird Promise style API.

--- test/apis/promise.js
+++ test/apis/promise.js
@@ -2,69 +2,163 @@
 
 const path = require('path');
 
-const {cayleyInstancePools, assert, expect, should} = require('./initialize')(true);
+const {cayleyClient, assert, expect, should} = require('./initialize')(true);
 
-const pickRandomly = cayleyInstancePools[0].pickRandomly;
+const testData = require('../data/friend_circle_with_label.nq.equivalent_json');
 
 describe('Cayley bluebird Promise style HTTP APIs', function() {
 
-  it('/write', function() {
-    return pickRandomly(pickRandomly(cayleyInstancePools)).write([
-      {
-        primaryKey: '</user/shortid/23TplPdS>',
-        label: 'companyA',
+  it('/v1/write & /v2/read & /v1/delete', function() {
+    /*
+     * Test API: '/v1/write'.
+     */
+    return cayleyClient.write(testData).then((writeResBody) => {
+      expect(writeResBody.result).to.be.a('string');
+      expect(writeResBody.result).to.include('Successfully');
 
-        userId: '23TplPdS',
-        userSetId: 'XXX_L2',
-        realName: 'XXX_L3',
-        displayName: 'XXX_L4',
-        gender: 'M',
-        email: 'xxx.l6@xxx.com',
-        isEmailVerified: false,
-        mobilePhone: {
-          isVerified: false,
-          alpha3CountryCode: '+65',
-          mobilePhoneNoWithCountryCallingCode: '+6586720011'
-        }
-      }
-    ]).then(function(resBody) {
-      expect(resBody.result).to.be.a('string');
-      expect(resBody.result).to.include('Successfully');
+      /*
+       * Test API: '/v2/read'.
+       */
+      return cayleyClient.read().then((readResBody) => {
+        assert.isArray(readResBody);
+        assert.equal(readResBody.length, 42);
+
+        /*
+         * Test API: '/v1/delete'.
+         *   Delete the tested inserted data.
+         */
+        return cayleyClient.delete(testData).then((deleteResBody) => {
+          expect(deleteResBody.result).to.be.a('string');
+          expect(deleteResBody.result).to.include('Successfully');
+
+          /*
+           * Use '/v2/read' API to verify.
+           */
+          return cayleyClient.read().then((readResBody) => {
+            assert.isNull(readResBody);
+          });
+        });
+      });
     });
   });
 
-  it('/write/file/nquad', function() {
-    return pickRandomly(pickRandomly(cayleyInstancePools)).writeFile(
-      path.resolve(__dirname, '../data/friend_circle_with_label.nq')
-    ).then(function(resBody) {
-      expect(resBody.result).to.be.a('string');
-      expect(resBody.result).to.include('Successfully');
-    });
-  });
+});
 
-  // https://github.com/cayleygraph/cayley/issues/517
-  it('/delete', function() {
-    return pickRandomly(pickRandomly(cayleyInstancePools)).delete([
+describe('Cayley bluebird Promise style HTTP APIs', function() {
+
+  it("'Write' with 'label', but 'delete' without providing 'label', expect deletion to be failed.", function() {
+    /*
+     * '/v1/write' data with 'label'.
+     */
+    return cayleyClient.write([
       {
-        primaryKey: '</user/shortid/23TplPdS>',
+        primaryKey: '</user/shortid/23TplPLs>',
         label: 'companyA',
 
-        userId: '23TplPdS',
-        userSetId: 'XXX_L2',
-        realName: 'XXX_L3',
-        displayName: 'XXX_L4',
-        gender: 'M',
-        email: 'xxx.l6@xxx.com',
+        userId: '23TplPLs',
         isEmailVerified: false,
         mobilePhone: {
           isVerified: false,
-          alpha3CountryCode: '+65',
+          alpha3CountryCode: 'SGP',
           mobilePhoneNoWithCountryCallingCode: '+6586720011'
         }
       }
-    ]).then(function(resBody) {
-      expect(resBody.result).to.be.a('string');
-      expect(resBody.result).to.include('Successfully');
+    ]).then((writeResBody) => {
+      expect(writeResBody.result).to.be.a('string');
+      expect(writeResBody.result).to.include('Successfully');
+
+      /*
+       * But try to use '/v1/delete' to delete data without providing 'label', expect deletion to be failed.
+       */
+      return cayleyClient.delete([
+        {
+          primaryKey: '</user/shortid/23TplPLs>',
+
+          userId: '23TplPLs',
+          isEmailVerified: false,
+          mobilePhone: {
+            isVerified: false,
+            alpha3CountryCode: 'SGP',
+            mobilePhoneNoWithCountryCallingCode: '+6586720011'
+          }
+        }
+      ]).then((wronglyDeleteResBody) => {
+
+        // After checked the MongoDB, cayley deleted nothing, but still returned: { result: 'Successfully deleted 11 quads.' }
+        // So we use the '/v2/read' API to check.
+
+        /*
+         * Check with '/v2/read'.
+         */
+        return cayleyClient.read().then((readResBody) => {
+          assert.isArray(readResBody);
+          assert.equal(readResBody.length, 6);
+
+          /*
+           * Delete the inserted data correctly with '/v1/delete'.
+           */
+          return cayleyClient.delete([
+            {
+              primaryKey: '</user/shortid/23TplPLs>',
+              label: 'companyA',
+
+              userId: '23TplPLs',
+              isEmailVerified: false,
+              mobilePhone: {
+                isVerified: false,
+                alpha3CountryCode: 'SGP',
+                mobilePhoneNoWithCountryCallingCode: '+6586720011'
+              }
+            }
+          ]).then((correctlyDeleteResBody) => {
+            expect(correctlyDeleteResBody.result).to.be.a('string');
+            expect(correctlyDeleteResBody.result).to.include('Successfully');
+
+            /*
+             * Use '/v2/read' to verify.
+             */
+            return cayleyClient.read().then((readResBody) => {
+              assert.isNull(readResBody);
+            });
+          });
+        });
+
+      });
+    });
+  });
+
+});
+
+describe('Cayley bluebird Promise style Gizmo APIs', function() {
+
+  before(function(done) {
+    cayleyClient.write(testData).then((res) => {
+      done();
+    }).catch((err) => {
+      done(err);
+    });
+  });
+
+  after(function(done) {
+    cayleyClient.delete(testData).then((res) => {
+      /*
+       * Use '/v2/read' to verify the test cases didn't pollute the user db.
+       */
+      return cayleyClient.read();
+    }).then((res) => {
+      if (res) {
+        done(new Error('Your db got polluted by this test, please report this issue!!!'));
+      } else {
+        done();
+      }
+    }).catch((err) => {
+      done(err);
+    });
+  });
+
+  it('graph.Vertex(nodeId)', function() {
+    return cayleyClient.g.V('</user/shortid/23TplPdS>').All().then((res) => {
+      assert.isArray(res.result);
     });
   });
 
